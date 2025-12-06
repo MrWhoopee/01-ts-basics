e42e968392e39c6e2b304f6c88a793779c99937d
Mr_Whoopee <artem.holovko.97@gmail.com>
2025-12-06 12:31:48 +0200

    Done

---
+++ src/task-6.ts
@@ -0,0 +1,10 @@
+console.log("\n   Task 6 \n");
+
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement<number>([1, 2, 3]);           // 1
+getFirstElement<string>(["a", "b", "c"]);     // "a"
+getFirstElement<boolean>([true, false, true]); // true
+// getFirstElement([1, "b", true]);
